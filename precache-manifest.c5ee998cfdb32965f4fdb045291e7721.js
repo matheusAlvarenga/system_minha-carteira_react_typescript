213701e3607408bbd23bb6ba2bcaabf089c25ec2
Matheus Alvarenga <matheus@hubway.com>
2021-05-18 18:30:50 -0300

    Updates

---
+++ precache-manifest.c5ee998cfdb32965f4fdb045291e7721.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e703498bc9a5213b660e37d6793f038",
+    "url": "/system_minha-carteira_react_typescript/index.html"
+  },
+  {
+    "revision": "221b0fcaaf560c6ee86a",
+    "url": "/system_minha-carteira_react_typescript/static/js/2.67f0a89a.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/system_minha-carteira_react_typescript/static/js/2.67f0a89a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f280e0336f12ce8ecb0e",
+    "url": "/system_minha-carteira_react_typescript/static/js/main.d4fdfad2.chunk.js"
+  },
+  {
+    "revision": "66240b0c4713a8ec4c38",
+    "url": "/system_minha-carteira_react_typescript/static/js/runtime-main.a5afe89d.js"
+  },
+  {
+    "revision": "81059ca205a82d7c4276c80c0a8d5568",
+    "url": "/system_minha-carteira_react_typescript/static/media/arrow-down.81059ca2.svg"
+  },
+  {
+    "revision": "ecbbd9cf6eb71246536590dd3a596f2b",
+    "url": "/system_minha-carteira_react_typescript/static/media/arrow-up.ecbbd9cf.svg"
+  },
+  {
+    "revision": "d56dac333231b66c2876ac37190de775",
+    "url": "/system_minha-carteira_react_typescript/static/media/dolar.d56dac33.svg"
+  },
+  {
+    "revision": "81ff6edb416dbfb92cff7f36394c6f8c",
+    "url": "/system_minha-carteira_react_typescript/static/media/grinning.81ff6edb.svg"
+  },
+  {
+    "revision": "eb82222585fd711a6e9a06700df496a1",
+    "url": "/system_minha-carteira_react_typescript/static/media/happy.eb822225.svg"
+  },
+  {
+    "revision": "7f611f0fa14dfd6718ccbf8a420a996e",
+    "url": "/system_minha-carteira_react_typescript/static/media/logo.7f611f0f.svg"
+  },
+  {
+    "revision": "6b1b1fa3a8467a6e092a5e248e5309b5",
+    "url": "/system_minha-carteira_react_typescript/static/media/ops.6b1b1fa3.svg"
+  },
+  {
+    "revision": "af596016322643905e591f48477a09d1",
+    "url": "/system_minha-carteira_react_typescript/static/media/sad.af596016.svg"
+  }
+]);
